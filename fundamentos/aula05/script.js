c3414debf133389976430e6994c9e4af1fa04882
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-06-25 23:29:33 -0300

    Variáveis e constantes - Aula 05

---
+++ fundamentos/aula05/script.js
@@ -0,0 +1,10 @@
+// Variáveis e constantes
+let altura = 1.70;
+let peso = 60.80;
+
+let imc = peso / (altura * altura);
+
+console.log(`Seu imc é de ${imc.toFixed(2)}`);
+
+const pi = 3.14;
+console.log(pi);
