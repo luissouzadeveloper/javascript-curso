0650151edf375ed54163e22ed326ee9c12850d2a
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-06-30 23:02:42 -0300

    Operadores

---
+++ operadores_funcoes/aula01.js
@@ -0,0 +1,19 @@
+// Operadores
+
+// Soma
+let n1 = 5;
+let n2 = 2;
+let resultado = n1 + n2;
+console.log(resultado);
+
+// Subtração
+resultado = n1 - n2;
+console.log(resultado);
+
+// Multiplicação
+resultado = n1 * n2;
+console.log(resultado);
+
+// Divisão
+resultado = n1 / n2;
+console.log(resultado);
