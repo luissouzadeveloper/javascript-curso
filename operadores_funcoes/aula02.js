ad2641fb37d44dcebd942f8453ae05a91e509c96
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-06-30 23:03:10 -0300

    Operadores realcionais e lógicos

---
+++ operadores_funcoes/aula02.js
@@ -0,0 +1,41 @@
+// Operadores relacionais
+
+// Igual -> == ou ===
+let resultado = 10 == 10;
+console.log(resultado);
+
+// Diferente -> !=
+resultado = 10 != 10;
+console.log(resultado);
+
+// Maior que -> >
+resultado = 10 > 4;
+console.log(resultado);
+
+// Menor que -> <
+resultado = 10 < 4;
+console.log(resultado);
+
+// Maior ou igual -> >=
+resultado = 10 >= 10;
+console.log(resultado);
+
+// Menor ou igual -> <=
+resultado = 10 <= 10;
+console.log(resultado);
+
+let idade = 16;
+let maiorIdade = idade > 18;
+console.log(maiorIdade);
+
+let pedidoMinimo = 20;
+let compra = 100;
+let frete = compra > pedidoMinimo;
+console.log(frete);
+
+// Operadores lógicos
+let teste = true && true;
+console.log(teste);
+
+teste = false || true;
+console.log(teste);
