504ce05cc2b9c58c529bd9218fbd1527786a6134
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-10-25 00:09:24 -0300

    Tipos de variáveis

---
+++ fundamentos/aula04/aula04.js
@@ -0,0 +1,21 @@
+// Tipos de variáveis
+
+// string
+let frase = 'Oi, tudo bem?';
+console.log(frase);
+
+// number
+let idade = 22.45;
+console.log(idade);
+
+// boolean
+let ligado = true;
+console.log(ligado);
+
+// undefined
+let numero;
+console.log(numero);
+
+// null
+let qntd = null;
+console.log(qntd);
