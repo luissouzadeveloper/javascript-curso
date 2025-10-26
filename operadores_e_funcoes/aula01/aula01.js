0ef718f0991b76236f41782a7ae19a7998160b30
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-10-26 09:05:25 -0300

    Operadores aritméticos

---
+++ operadores_e_funcoes/aula01/aula01.js
@@ -0,0 +1,23 @@
+// Operadores aritméticos
+
+/*
+    Soma (+)
+    Subtração (-)
+    Multiplicação (*)
+    Divisão (/)
+*/
+
+let n1 = 10;
+let n2 = 3;
+
+let soma = n1 + n2;
+console.log(soma);
+
+let subtracao = n1 - n2;
+console.log(subtracao);
+
+let multiplicacao = n1 * n2;
+console.log(multiplicacao);
+
+let divisao = n1 / n2;
+console.log(divisao);
