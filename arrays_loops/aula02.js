726d5a8ec7d58716ab03a985624c55a333764d3f
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-11-03 23:23:58 -0300

    Concatenação e template strings

---
+++ arrays_loops/aula02.js
@@ -0,0 +1,9 @@
+// Concatenação e template strings
+const nome = 'Luis';
+const idade = 18;
+
+console.log(nome + ' ' + idade);
+console.log('Bem vindo ' + nome);
+
+const texto = `Olá ${nome}, você tem ${idade} anos!`;
+console.log(texto);
