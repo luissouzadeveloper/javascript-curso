4b6952292208e9846ee91bbea5b789c085311c41
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-11-24 12:04:36 -0300

    Operadores de atribuição

---
+++ arrays_loops/aula05.js
@@ -0,0 +1,12 @@
+// Operador de atribuição =
+
+let salario = 800;
+let bonus = 150;
+
+// salario = salario + bonus;
+salario += bonus;
+console.log(salario);
+
+let desconto = 400;
+salario -= desconto;
+console.log(salario);
