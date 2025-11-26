0894878ef6d2df08d8484d9b33eb26bcf26419ff
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-11-25 22:56:38 -0300

    Diferenças entre var e let

---
+++ arrays_loops/aula07.js
@@ -0,0 +1,27 @@
+// // var vs let
+
+// // escopo global
+// var numero = 10;
+
+// {
+//     numero = 5;
+//     console.log(numero);
+// }
+
+// console.log(numero);
+
+// let numero2 = 15;
+
+// {
+//     numero2 = 10;
+//     console.log(numero2);
+// }
+
+// console.log(numero2);
+
+{
+    let valor = 5;
+    console.log(valor);
+}
+
+console.log(valor);
