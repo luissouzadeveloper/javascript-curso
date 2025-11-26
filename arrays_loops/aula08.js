88129ab73af8ed12045b274cde37edbebec1df49
Luis Felipe <luisfelipeosouzati@gmail.com>
2025-11-26 09:09:12 -0300

    Funções anônimas e arrow function

---
+++ arrays_loops/aula08.js
@@ -0,0 +1,13 @@
+// Funções anônimas e arrow function
+const somar = function(v1, v2) {
+    console.log(v1 + v2)
+}
+somar(5,2);
+
+const multiplicar = (v1, v2) => {
+    console.log(v1 * v2);
+}
+multiplicar(5,2);
+
+const dividir = (v1, v2) => console.log(v1 / v2);
+dividir(10, 2);
