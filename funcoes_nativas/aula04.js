d5cf4db24a50739040e711a4d5613f9f0164e971
Luis Felipe <luisfelipeosouzati@gmail.com>
2026-02-27 10:26:42 -0300

    Funções nativas matemáticas

---
+++ funcoes_nativas/aula04.js
@@ -0,0 +1,29 @@
+// Funções nativas para matemática
+let numero = 276.75;
+
+// PI
+console.log(Math.PI);
+
+// ceil -> arredonda pra cima
+console.log(Math.ceil(numero));
+
+// floor -> arredonda pra baixo
+console.log(Math.floor(numero));
+
+// round -> arredonda conforme a parte decimal
+console.log(Math.round(numero));
+
+// random -> gera número aleatório entre 0 e 1
+console.log(Math.random(numero));
+
+// min -> pega o menor valor
+console.log(Math.min(2,7));
+
+// max -> pega o maior valor
+console.log(Math.max(2,7));
+
+// pow -> potência
+console.log(Math.pow(5, 2));
+
+// sqrt -> retorna raiz quadrada
+console.log(Math.sqrt(144));
