2a8efea347fda3f63716907bfe042fd8717b1c7b
Luis Felipe <luisfelipeosouzati@gmail.com>
2026-03-03 09:07:25 -0300

    Arrays - saiba mais

---
+++ arrays_intermediario/aula01.js
@@ -0,0 +1,17 @@
+// Arrays intermediário
+const lista = [
+    'arroz',
+    'feijão',
+    'frango'
+];
+
+console.log(lista[1]);
+
+const nomes = ['joão', 'maria', 'pedro'];
+nomes.push('miguel');
+delete nomes[0];
+console.log(nomes);
+
+const numeros = [10, 7, 2, 0, 20, 34];
+numeros.sort();
+console.log(numeros);
