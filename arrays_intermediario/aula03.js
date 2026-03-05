4227b097f9475198dddf7eb702da095500267b0d
Luis Felipe <luisfelipeosouzati@gmail.com>
2026-03-05 12:38:39 -0300

    Array foreach

---
+++ arrays_intermediario/aula03.js
@@ -0,0 +1,15 @@
+// Array foreach
+const lista = ['João', 'Zé', 'Maria', 'Raimunda']; 
+
+lista.forEach((item, i, arr) => {
+    console.log(item)
+});
+
+const frutas = [
+    'maçã',
+    'uva',
+    'laranja',
+    'goiaba'
+];
+
+frutas.forEach((item) => console.log(item));
